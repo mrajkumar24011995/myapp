00eb893c9cd94672fd053f8c1de742fb36fd09e3
cipher <fynhacker@gmail.com>
2023-02-21 18:40:30 +0530

    vaild

--- src/components/AddStudents.js
+++ src/components/AddStudents.js
@@ -1,3 +1,5 @@
+
+
 import React from "react";
 import {data} from "../Data/data"
 
--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -1,17 +1,22 @@
 import { Button } from '@mui/material'
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import Base from '../Base/Base'
 
 const DashBoard = () => {
+    const history = useHistory();
     return(
        <Base
        tittle = "Welcome to Students App"
        description= "Please click below buton to home page"
        >
+        <br/>
         <Button variant='outlined' 
         color='red'
-        size ="large">
-            Home
+        size ="large"
+        onClick={()=>history.push("/details")}
+        >
+        Students Page
         </Button>
 
        </Base>
